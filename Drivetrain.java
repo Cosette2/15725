fed377862708d55284a15aa0a8b142b662280e6c
Cosette2 <kylezheng27@gmail.com>
2024-09-10 17:13:46 -0500

    Add files via upload

--- Drivetrain.java
+++ Drivetrain.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.robotcontroller;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+// suck my dick nigger 
 
 @TeleOp
 public class Drivetrain extends LinearOpMode {
